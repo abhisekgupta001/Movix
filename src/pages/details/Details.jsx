85ba5c04de97858a91fca790ebe27782b553611e
Abhisek Gupta <guptaabhi6488@gmail.com>
2024-01-27 19:44:07 +0530

    completed details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,10 @@ import { useParams } from "react-router-dom";
 import "./style.scss";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+import Recommendation from "./carousels/Recommendation";
+import Similar from "./carousels/Similar";
 
 const Details = () => {
     const {mediaType, id} = useParams();
@@ -11,6 +15,11 @@ const Details = () => {
 
   return <div>
     <DetailsBanner video={data?.results[0]} crew={credits?.crew}/>
+    <Cast data={credits?.cast} loading={creditsLoading} />
+    <VideosSection data={data} loading={loading}/>
+    <Similar mediaType={mediaType} id={id}/>
+    <Recommendation mediaType={mediaType} id={id} />
+    
   </div>;
 };
 
