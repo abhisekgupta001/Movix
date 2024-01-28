3f6055aa39bb76f4b39b7f4b1346012c7ebe4f5e
Abhisek Gupta <guptaabhi6488@gmail.com>
2024-01-28 17:18:44 +0530

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    Movix is a react app that leverage the TMDB API and display details of movies and TV Shows. It shows the trending movies, the top rated movies, their casting and crews, their IMDB rating. You can also see the trailers or other videos associated with the movie or tvshow.
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
